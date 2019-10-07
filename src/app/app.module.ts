6cc1fa821e81123f96d5d1a787c34112eba5e134
Vincent Goay <vincentgoay@gmail.com>
2019-10-07 10:27:28 +0800

    Initialize form and albums component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form.component';
+import { AlbumComponent } from './components/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule
